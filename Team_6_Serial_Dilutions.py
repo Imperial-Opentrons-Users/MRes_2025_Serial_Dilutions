e69081c7229bf7c0f8f5137761adaac02544ef5c
Wilson Porteus <103076735+pineapplefart@users.noreply.github.com>
2025-11-27 11:02:52 +0000

    Update authors in metadata and clean up comments

--- Team_6_Serial_Dilutions.py
+++ Team_6_Serial_Dilutions.py
@@ -5,12 +5,12 @@ metadata = {
     "apiLevel": "2.15",
     "protocolName": "Serial Dilutions",
     "description": "This protocol is the outcome of the iGEM Serial Dilution Standard Protocol",
-    "author": "Wilson et al"
+    "author": "Wilson, Haotong, Matt"
     }
 
 def run(protocol: protocol_api.ProtocolContext):
         #protocol = simulate.get_protocol_api('2.15')
-
+    #Parameters
         plate = protocol.load_labware('costar3370flatbottomtransparent_96_wellplate_200ul', 3)
         tiprack_1 = protocol.load_labware('opentrons_96_tiprack_300ul', 1)
         p300 = protocol.load_instrument('p300_multi_gen2', 'left', tip_racks = [tiprack_1])
@@ -28,12 +28,10 @@ def run(protocol: protocol_api.ProtocolContext):
         fluorescein_volume = 200     
         pbs_volume = 100             
 
-
         dilution_volume = 100        
         mix_volume = 150             
 
         res_asp_height = 1.5
-
         plate_asp_height = 1.0
         plate_disp_height = 1.0
 
@@ -41,16 +39,13 @@ def run(protocol: protocol_api.ProtocolContext):
         mix_mid_height = 1.5
         mix_high_height = 2.5
 
-
         fluorescein_src = reservoir['A1']   
         pbs_src = reservoir['A2']           
         waste = reservoir['A12']   
                 
-
-
         #rows = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H']
 
-        #Fluorescein
+    #Fluorescein
         p300.pick_up_tip()
         p300.aspirate(fluorescein_volume, fluorescein_src.bottom(res_asp_height), rate=slow)
         p300.dispense(fluorescein_volume, plate['A1'], rate=slow)
@@ -62,8 +57,6 @@ def run(protocol: protocol_api.ProtocolContext):
         for col in range(2,13):  
                 #top_row=plate.rows()[0]
                 dest = plate[f'A{col}']
-
-               
 
                 p300.aspirate(
                 pbs_volume,
@@ -80,17 +73,12 @@ def run(protocol: protocol_api.ProtocolContext):
                 p300.blow_out(dest.top())
         p300.drop_tip()
 
-
-
-       
-
-        #Dilutions
+    #Dilutions
         p300.pick_up_tip()
         for col in range(1,11):
                 
                 source = plate[f'A{col}']       
                 dest = plate[f'A{col + 1}']     
-
 
                 p300.aspirate(
                         dilution_volume,
@@ -98,12 +86,10 @@ def run(protocol: protocol_api.ProtocolContext):
                         rate= p300.flow_rate.aspirate
                 )
 
-
                 p300.dispense(
                         dilution_volume,
                         dest.bottom(plate_disp_height),
-                        rate=p300.flow_rate.dispense,
-                        
+                        rate=p300.flow_rate.dispense,        
                 )
 
                 p300.aspirate(mix_volume,dest.bottom(mix_low_height),rate=normal)
